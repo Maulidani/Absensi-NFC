04793786997d702b6c8136f5aae025ecaec27176
maulidani <maulidanimahmud@gmail.com>
2022-10-27 15:30:20 +0800

    update

--- app/src/main/java/domain/skripsi/absensinfc/ui/ScanNFCActivity.kt
+++ app/src/main/java/domain/skripsi/absensinfc/ui/ScanNFCActivity.kt
@@ -1,12 +1,69 @@
 package domain.skripsi.absensinfc.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.NfcA
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import domain.skripsi.absensinfc.R
 
 class ScanNFCActivity : AppCompatActivity() {
+
+    private var nfcAdapter: NfcAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_nfc)
+
+        this.nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val tagFromIntent: Tag? = intent?.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+        val nfc = NfcA.get(tagFromIntent)
+
+        val atqa: ByteArray = nfc.atqa
+        val sak: Short = nfc.sak
+
+        nfc.connect()
+        val isConnected = nfc.isConnected
+
+        if (isConnected) {
+            val receivedData: ByteArray = nfc.transceive(atqa)
+            //code to handle the received data
+            // Received data would be in the form of a byte array that can be converted to string
+            //NFC_READ_COMMAND would be the custom command you would have to send to your NFC Tag in order to read it
+        } else {
+            Log.e("ans", "Not connected")
+        }
+    }
+
+    private fun enableForegroundDispatch(activity: AppCompatActivity, adapter: NfcAdapter?) {
+        val intent = Intent(activity.applicationContext, activity.javaClass)
+        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        val pendingIntent = PendingIntent.getActivity(activity.applicationContext, 0, intent, 0)
+        val filters = arrayOfNulls<IntentFilter>(1)
+        val techList = arrayOf<Array<String>>()
+        filters[0] = IntentFilter()
+        with(filters[0]) {
+            this?.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED)
+            this?.addCategory(Intent.CATEGORY_DEFAULT)
+            try {
+                this?.addDataType("text/plain")
+            } catch (ex: IntentFilter.MalformedMimeTypeException) {
+                throw RuntimeException(ex)
+            }
+        }
+        adapter?.enableForegroundDispatch(activity, pendingIntent, filters, techList)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        enableForegroundDispatch(this, this.nfcAdapter)
     }
 }
