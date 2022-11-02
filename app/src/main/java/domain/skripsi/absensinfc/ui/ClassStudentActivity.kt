adc45ca06f2f29fc274c0a1691cb563de356b964
maulidani <maulidanimahmud@gmail.com>
2022-11-03 00:12:32 +0800

    update

---
+++ app/src/main/java/domain/skripsi/absensinfc/ui/ClassStudentActivity.kt
@@ -0,0 +1,12 @@
+package domain.skripsi.absensinfc.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import domain.skripsi.absensinfc.R
+
+class ClassStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class_student)
+    }
+}
